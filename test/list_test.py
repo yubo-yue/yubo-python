94497337ce2ec89149049ee35abbd95aa13d25cd
Yu.Yue <yueyu@xiaomi.com>
2015-05-15 17:13:31 +0800

    control flow study

---
+++ test/list_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+x = int(input("pls enter an integer: "))
+if x < 0:
+    x = 0
+    print('Negative chagned to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+words = ["cat"]
